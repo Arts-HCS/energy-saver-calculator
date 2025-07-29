64f56c65dd3e57c72720e718914443587c7fa1b1
Arturo Sandoval <artssc925@gmail.com>
2025-07-28 19:35:37 -0600

    Small changes in main.py

--- calculator_esp-main/main.py
+++ calculator_esp-main/main.py
@@ -1,22 +1,18 @@
-# Importar
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 def result_calculate(size, lights, device):
-    # Variables que permiten calcular el consumo energético de los aparatos
     home_coef = 100
     light_coef = 0.04
     devices_coef = 5   
     return size * home_coef + lights * light_coef + device * devices_coef 
 
-# La primera página
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# La segunda página
 @app.route('/<size>')
 def lights(size):
     return render_template(
@@ -24,7 +20,6 @@ def lights(size):
                             size=size
                            )
 
-# La tercera página
 @app.route('/<size>/<lights>')
 def electronics(size, lights):
     return render_template(
@@ -33,7 +28,6 @@ def electronics(size, lights):
                             lights = lights                           
                            )
 
-# Cálculo
 @app.route('/<size>/<lights>/<device>')
 def end(size, lights, device):
     return render_template('end.html', 
